534421fe6055c92d7092a6ce8eca66b80bbc76e3
thangasujithas <48404618+thangasujithas@users.noreply.github.com>
2024-02-14 16:23:49 +0530

    merge audio and video

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI, Request, UploadFile, File
-from moviepy.editor import ImageClip, concatenate_videoclips, TextClip
+from moviepy.editor import *
 from gtts import gTTS
 import os
+import json, openai, pandas
+import numpy as np
+import warnings
+import os
+import moviepy.video.io.ImageSequenceClip
+import pygame
+warnings.filterwarnings('ignore')
+import configparser
 
 from requests import request
 
@@ -9,40 +17,24 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    # text = "Hello, this is a test."
-    # tts = gTTS(text=text, lang='en')
-    # tts.save("output.mp3")
-    # os.system("output.mp3")
-    return {"message": "Hello World"}
-
-def create_video(images_folder, text, output_path):
-    # Get all image files from the folder
-    image_files = sorted([f for f in os.listdir(images_folder) if f.endswith(('.png', '.jpg', '.jpeg'))])
-    
-    # Create ImageClips from images
-    clips = [ImageClip(os.path.join(images_folder, img)).set_duration(3) for img in image_files]
-
-    # Add text narration
-    text_clip = TextClip(text, fontsize=30, color='white').set_duration(len(clips) * 3)
-    text_clip = text_clip.set_pos(('center', 'bottom'))
-
-    # Concatenate ImageClips and text clip
-    final_clip = concatenate_videoclips(clips)
-    final_clip = final_clip.set_audio(text_clip.audio)
-
-    # Write the final video to output_path
-    final_clip.write_videofile(output_path, codec='libx264', fps=24)
+    return {"message": "Welcome"}
 
 @app.post("/generate_video")
 async def generate_video(request: Request) :
     try:
-        data = await request.json()
-        output_path = "output_video.mp4"
-        
-        text =  data.get("text", "Default text if not provided in the request")
-        images_folder = data.get("images_folder", None)
-        with open(images_folder, "wb") as buffer:
-            buffer.write(images_folder.file.read())
-        create_video(images_folder, text, output_path)
+       return images_to_video('C:\\Users\\Admin\\projects\\hackathon_team3\\hackathon_team3\\images_new', 54, '.jpeg', 'Spectra_video', '.mp4')
     except Exception as e:
         return str(e)
+    
+def images_to_video(image_folder_path: str, fps, extension:str, video_name:str, output_format:str):
+    images = []
+    print(image_folder_path)
+    for img in os.listdir(image_folder_path):
+        images.append(img)
+    images = ['0.jpg', '11.jpg']
+    movie_clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(images, fps)
+    movie_clip.write_videofile(video_name+output_format)
+    video_clip = VideoFileClip("Spectra_video.mp4")
+    audio_clip = AudioFileClip("output.mp3")
+    final_clip = video_clip.set_audio(audio_clip)
+    final_clip.write_videofile("youtube_v" + ".mp4")	
